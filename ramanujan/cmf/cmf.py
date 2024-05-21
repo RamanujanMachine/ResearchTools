b55f84027f382c749ac5b0815f0e4d0bb368616b
Shachar Weinbaum <shacharwein@gmail.com>
2024-05-21 20:00:26 +0300

    Feature - Trajectory_scan: scans trajectories and returns useful information     Given a CMF, a List of trajectories, and a starting point, this function:     Calculates the trajectory matrix of each trajectory in the list     converts the matrix to a PCF     Modifies the starting point as to prevent singularities     calculates the limit and the delta of the PCF     returns the limit, precision, delta, pcf, and starting point for each trajectory.

--- ramanujan/cmf/cmf.py
+++ ramanujan/cmf/cmf.py
@@ -274,22 +274,20 @@ class CMF:
         extracts and returns useful information from a trajectory of a CMF.
         Given a CMF, a trajectory, and a starting point, this function:
         Calculates the trajectory matrix
-        Modifies the starting point as to prevent singularities
         converts the matrix to a PCF
+        Modifies the starting point as to prevent singularities
         calculates the limit and the delta of the PCF
         returns the limit, precision, delta, pcf, and starting point for the trajectory.
         """
         Mtraj = self.trajectory_matrix(traj, start) #traj matrix
-        MtrajDet = Mtraj.det() #find Determinant
         Mtraj_denom = sp.lcm_list([sp.denom(elem) for elem in Mtraj]) #find Denominator
-        sing_list = sp.solve(sp.denom(MtrajDet))+sp.solve(sp.numer(MtrajDet))+sp.solve(Mtraj_denom) # list of singular offets.
+        Mtraj_norm = (Mtraj*Mtraj_denom).normalize()
+        traj_pcf = Mtraj_norm.as_pcf().pcf
+        sing_list = sp.solve(traj_pcf.a_n)+sp.solve(traj_pcf.b_n) # list of singular offets.
         offset = max(0,max([r for r in sing_list if r.is_integer])) # finds maximal integer root over 0.
         if offset!=0:
             start = {k:(start[k]+offset*traj[k]) for k in self.axes()}
-            Mtraj = Mtraj.subs({n:n+offset})
-            Mtraj_denom = Mtraj_denom.subs({n:n+offset})
-        Mtraj_norm = (Mtraj*Mtraj_denom).normalize()
-        traj_pcf = Mtraj_norm.as_pcf().pcf
+            traj_pcf = traj_pcf.subs({n:n+offset})
         lim_obj = traj_pcf.limit(iterations)
         lim = lim_obj.as_rounded_number()
         prec = lim_obj.precision()
