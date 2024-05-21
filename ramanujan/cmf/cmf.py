78f78854116497394a327d180e9b21eec069b832
Shachar Weinbaum <shacharwein@gmail.com>
2024-05-21 19:24:44 +0300

    Feature - Trajectory_scan: scans trajectories and returns useful information     Given a CMF, a List of trajectories, and a starting point, this function:     Calculates the trajectory matrix of each trajectory in the list     Modifies the starting point as to prevent singularities     converts the matrix to a PCF     calculates the limit and the delta of the PCF     returns the limit, precision, delta, pcf, and starting point for each trajectory.

--- ramanujan/cmf/cmf.py
+++ ramanujan/cmf/cmf.py
@@ -256,42 +256,42 @@ class CMF:
 
 
 
-def trajectory_scan(self,traj_space, start, iterations = 100):
-    r"""
-    scans trajectories and returns useful information
-    Given a CMF, a List of trajectories, and a starting point, this function:
-    Calculates the trajectory matrix of each trajectory in the list
-    Modifies the starting point as to prevent singularities
-    converts the matrix to a PCF
-    calculates the limit and the delta of the PCF
-    returns the limit, precision, delta, pcf, and starting point for each trajectory.
-    """
-    return [self.analyze_trajectory(traj,start,iterations) for traj in traj_space]
+    def trajectory_scan(self,traj_space, start, iterations = 100):
+        r"""
+        scans trajectories and returns useful information
+        Given a CMF, a List of trajectories, and a starting point, this function:
+        Calculates the trajectory matrix of each trajectory in the list
+        Modifies the starting point as to prevent singularities
+        converts the matrix to a PCF
+        calculates the limit and the delta of the PCF
+        returns the limit, precision, delta, pcf, and starting point for each trajectory.
+        """
+        return [self.analyze_trajectory(traj,start,iterations) for traj in traj_space]
 
 
-def analyze_trajectory(self,traj, start, iterations=100):
-    r"""
-    extracts and returns useful information from a trajectory of a CMF.
-    Given a CMF, a trajectory, and a starting point, this function:
-    Calculates the trajectory matrix
-    Modifies the starting point as to prevent singularities
-    converts the matrix to a PCF
-    calculates the limit and the delta of the PCF
-    returns the limit, precision, delta, pcf, and starting point for the trajectory.
-    """
-    Mtraj = C.trajectory_matrix(traj, start) #traj matrix
-    MtrajDet = Mtraj.det() #find Determinant
-    Mtraj_denom = sp.lcm_list([sp.denom(elem) for elem in Mtraj]) #find Denominator
-    sing_list = sp.solve(sp.denom(MtrajDet))+sp.solve(sp.numer(MtrajDet))+sp.solve(Mtraj_denom) # list of singular offets.
-    offset = max(0,max([r for r in sing_list if r.is_integer])) # finds maximal integer root over 0.
-    if offset!=0:
-        start = {k:(start[k]+offset*traj[k]) for k in C.axes()}
-        Mtraj = Mtraj.subs({n:n+offset})
-        Mtraj_denom = Mtraj_denom.subs({n:n+offset})
-    Mtraj_norm = (Mtraj*Mtraj_denom).normalize()
-    traj_pcf = Mtraj_norm.as_pcf().pcf
-    lim_obj = traj_pcf.limit(iterations)
-    lim = lim_obj.as_rounded_number()
-    prec = lim_obj.precision()
-    delta = traj_pcf.delta(iterations)
-    return (start,traj_pcf,lim,prec,delta)
+    def analyze_trajectory(self,traj, start, iterations=100):
+        r"""
+        extracts and returns useful information from a trajectory of a CMF.
+        Given a CMF, a trajectory, and a starting point, this function:
+        Calculates the trajectory matrix
+        Modifies the starting point as to prevent singularities
+        converts the matrix to a PCF
+        calculates the limit and the delta of the PCF
+        returns the limit, precision, delta, pcf, and starting point for the trajectory.
+        """
+        Mtraj = self.trajectory_matrix(traj, start) #traj matrix
+        MtrajDet = Mtraj.det() #find Determinant
+        Mtraj_denom = sp.lcm_list([sp.denom(elem) for elem in Mtraj]) #find Denominator
+        sing_list = sp.solve(sp.denom(MtrajDet))+sp.solve(sp.numer(MtrajDet))+sp.solve(Mtraj_denom) # list of singular offets.
+        offset = max(0,max([r for r in sing_list if r.is_integer])) # finds maximal integer root over 0.
+        if offset!=0:
+            start = {k:(start[k]+offset*traj[k]) for k in self.axes()}
+            Mtraj = Mtraj.subs({n:n+offset})
+            Mtraj_denom = Mtraj_denom.subs({n:n+offset})
+        Mtraj_norm = (Mtraj*Mtraj_denom).normalize()
+        traj_pcf = Mtraj_norm.as_pcf().pcf
+        lim_obj = traj_pcf.limit(iterations)
+        lim = lim_obj.as_rounded_number()
+        prec = lim_obj.precision()
+        delta = traj_pcf.delta(iterations)
+        return (start,traj_pcf,lim,prec,delta)
